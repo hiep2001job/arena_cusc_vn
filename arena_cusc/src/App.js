e836b3b85b2c0473a806e9b66813b1977103ec9d
Hiep <hiep2001.job@gmail.com>
2022-07-19 20:21:37 +0700

    Init react and tailwindcss

---
+++ arena_cusc/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      {/* <Banner/>
+      <Slides/> */}
+    </>
+  );
+}
+
+export default App;
---
+++ arena_cusc/src/components/Header/index.js
@@ -0,0 +1,9 @@
+function Header({}) {
+    return ( 
+        <h1 className="text-2xl underline">
+            Header ne
+        </h1>
+     );
+}
+
+export default Header;
