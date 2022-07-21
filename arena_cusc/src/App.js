89144e021e0955adb3be87ab3ac13f4fdaedb0b4
sontanhoang2001 <sontanhoang2001@gmail.com>
2022-07-21 20:38:39 +0700

    Created slide section

--- arena_cusc/src/App.js
+++ arena_cusc/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Header from './components/Header';
 import Slide from './components/Slide';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 function App() {
   return (
     <>
-      <Header />
-      {/* <Banner/>
-      <Slides/> */}
-
+      {/* <Header /> */}
       <Slide />
     </>
   );
