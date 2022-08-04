a0c53bc3f4d765e12dfe21b7bb55f375e8de2a6c
ntdienthanh2002 <phuoctamduyen151@gmail.com>
2022-08-04 13:15:01 +0700

    created Why Arena

--- arena_cusc/src/App.js
+++ arena_cusc/src/App.js
@@ -1,50 +1,44 @@
 import './App.css';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Slide from './components/Slide';
-<<<<<<< HEAD
-import CallToAction from './components/CallToAction';
+// import Header from './components/Header';
+// import Banner from './components/Banner';
+// import Slide from './components/Slide';
+// import CallToAction from './components/CallToAction';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Curriculum from './components/Curriculum';
-import GoodStudentExamples from './components/GoodStudentExamples';
-import Instructors from './components/Instructors';
-import News from './components/News';
-=======
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+// import Curriculum from './components/Curriculum';
+// import GoodStudentExamples from './components/GoodStudentExamples';
+// import Instructors from './components/Instructors';
+// import News from './components/News';
 import Footer from './components/Footer';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
 import WhyArena from './components/WhyArena';
 import AdmissionInfo from './components/AdmissionInfo';
-import GoodStudentExamples from './components/GoodStudentExamples';
->>>>>>> thanh
+// import GoodStudentExamples from './components/GoodStudentExamples';
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Header />
+      {/* <Header />
       <Banner />
       <Slide />
       <CallToAction/>
       <Instructors/>
       <GoodStudentExamples /> 
-      <News/>
+      <News/> */}
  
-=======
       {/* <Header/>
       <Banner/>      
       <Slide /> */}
-      {/* <WhyArena/> */}
-      {/* <h1>OK</h1>
+      <WhyArena/>
+      
       {/* <AdmissionInfo/> */}
       {/* <GoodStudentExamples/> */}
-      <div className='bg-red-500'>content ne</div>
-      <Footer/>
+      {/* <div className='bg-red-500'>content ne</div> */}
+      {/* <Footer/> */}
       {/* <h1>OK</h1> */}
->>>>>>> thanh
     </>
   );
 }
