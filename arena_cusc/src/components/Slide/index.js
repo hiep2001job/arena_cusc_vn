f99769da7fbc56b7ef68eda0da04a8837bed9d14
sontanhoang2001 <sontanhoang2001@gmail.com>
2022-08-17 10:59:01 +0700

    render data from sheet api

--- arena_cusc/src/components/Slide/index.js
+++ arena_cusc/src/components/Slide/index.js
@@ -6,15 +6,34 @@ import 'slick-carousel/slick/slick-theme.css';
 import './Slide.css';
 
 import Description from '../shared/Description';
+import apispreadsheets from '../shared/ApiSpreadSheets';
 
-import spiderman from '../../assets/images/slide/spider-man.jpg';
-import doctorStrange from '../../assets/images/slide/doctor-strange.jpg';
-import squidGame from '../../assets/images/slide/squildGame.png';
-import milion from '../../assets/images/slide/minion.jpg';
+function Slide() {
+  const [isLoaded, setIsLoaded] = useState(false);
 
-function Slide() { 
-  const slideObj = [spiderman, doctorStrange, squidGame, milion];
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [images, setImages] = useState([]);
+
   const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const sheetName = 'slide';
+    fetch(`https://opensheet.elk.sh/${apispreadsheets}/${sheetName}`)
+      .then((res) => res.json())
+      .then(
+        (results) => {
+          setIsLoaded(true);
+          setTitle(results[0].tieu_de);
+          setDescription(results[0].mo_ta);
+          setImages(results);
+        },
+        (error) => {
+          console.log('error: ', error);
+          setIsLoaded(false);
+        },
+      );
+  }, []);
 
   const settings = {
     className: 'center',
@@ -74,35 +93,22 @@ function Slide() {
       <div className="w-[100%] h-[900px] md:h-[1200px] relative mt-[-25%] md:mt-[-45%] z-[1]">
         <div
           className="w-[100%] bg-slide px-5 md:px-28 md:bg-[length:100%_100%]"
-          style={{ backgroundImage: `url(${slideObj[currentImage]})` }}
+          style={{ backgroundImage: `url(${isLoaded && images[currentImage].hinh_anh})` }}
         >
           <div className="overlay-slide"></div>
           <div className="relative">
             <h1 className="md:text-3xl font-bold text-center uppercase text-[yellow] mt-40 md:mt-[14rem] lg:mt-[19rem] xl:mt-[28rem]">
-              Học Arena, vương xa sáng tạo
+              {title}
             </h1>
-
-            <Description className="mt-5 text-white">
-              Nhu cầu càng ngày tăng của ngành truyền thông và giải trí tại Việt Nam là tiềm năng thức đẩy các sản phẩm
-              sáng tạo của ARENA MULTIMEDIA trong mảng thiết kế 3D để sáng tạo nên các nhân vật game ấn tượng, hay trong
-              bộ phim hoạt hình 3D với kỹ xão tuyệt đỉnh, ...xuất hiện, phát triển ở khắp nơi và có ý nghĩa vô cùng quan
-              trọng đối với cuộc sống. Bạn đã sẵn sàng cùng CUSC ARENA bước chân vào con đường sáng tạo ra những sản
-              phẩm tuyệt vời nảy chưa?
-            </Description>
+            <Description className="mt-5 text-white">{description}</Description>
           </div>
+
           <Slider {...settings} className="mt-5 md:mt-0">
-            <div className="img-slide">
-              <img className="rounded-3xl" src={spiderman} alt="1" />
-            </div>
-            <div className="img-slide">
-              <img className="rounded-3xl" src={doctorStrange} alt="2" />
-            </div>
-            <div className="img-slide">
-              <img className="rounded-3xl" src={squidGame} alt="3" />
-            </div>
-            <div className="img-slide">
-              <img className="rounded-3xl" src={milion} alt="4" />
-            </div>
+            {images.map((image, index) => (
+              <div key={index} className="img-slide">
+                <img className="rounded-3xl" src={image.hinh_anh} alt={image.tieu_de} />
+              </div>
+            ))}
           </Slider>
         </div>
       </div>
