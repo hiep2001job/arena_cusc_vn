f4dbf11077f7be44e60b30d63afbb35d7c0a2bc9
A18156 <Laughingnguyen15@gmail.com>
2022-08-25 12:15:15 +0700

    Fix responsive student's products

--- arena_cusc/src/components/Products/index.js
+++ arena_cusc/src/components/Products/index.js
@@ -4,8 +4,9 @@ import './products.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Title from '../shared/Title';
+import Popup from 'reactjs-popup';
+import ContentModal from '../shared/PopUpModal';
 import Button from '../shared/Button';
-import Description from '../shared/Description';
 import testImg_1 from '../../assets/images/san_pham_cua_sinh_vien/yasuo.jpg';
 import testImg_2 from '../../assets/images/san_pham_cua_sinh_vien/3Dcharactercover.jpg';
 import testImg_3 from '../../assets/images/3D_Game_Character.png';
@@ -280,6 +281,7 @@ const data = [
 ];
 
 function Products() {
+  const [closeModal, setcloseModal] = useState(false);
   const settings = {
     className: 'section-outstanding__slider',
     slidesToShow: 3,
@@ -353,7 +355,7 @@ function Products() {
                 }}
               >
                 <div
-                  className={`group-name uppercase absolute w-full text-center uppercase md:text-base font-bold sm:p-2.5 md:p-0`}
+                  className={`group-name uppercase absolute w-full text-center uppercase md:text-base font-bold cursor-pointer sm:p-2.5 md:p-10px`}
                 >
                   {group.groupName}
                 </div>
@@ -362,7 +364,7 @@ function Products() {
           </ul>
           {/* horizontal line*/}
           <div>
-            <div className="process-bar-line w-full m-auto bg-primary-color h-1 absolute top-[48px] md:block sm:hidden"></div>
+            <div className="process-bar-line w-full m-auto bg-primary-color h-1 absolute top-[62px] md:block sm:hidden"></div>
           </div>
         </div>
         {/* Content */}
@@ -371,22 +373,55 @@ function Products() {
             <Slider {...settings}>
               {/* start slide image */}
               {data[activeGroup].slides.map((slide, index) => (
-                <div key={`slide${index}`}>
-                  <div className="box rounded-[20px] mx-[20px] my-[15px] overflow-hidden relative rounded-[20px] xl:h-[250px] lg:h-[200px] sm:h-[220px] shadow-[0_0_10px_0px_#adb5bd]">
-                    <img
-                      className="product-slide-img rounded-[20px] bg-[#fdf34e] w-full h-full align-middle object-cover"
-                      src={`${slide.img}`}
-                      alt={slide.title}
-                    />
-                    <div className="overlay w-full h-full top-1/2 left-1/2 absolute translate-x-[-50%] translate-y-[-50%] rounded-[20px]">
-                      <div className="pl-[10%]">
-                        <h3 className="text-[#fdf34e] text-base">{slide.title}</h3>
-                        <p className="text-[#fdf34e] text-base">{slide.subTitle}</p>
-                        <p className="text-[#fdf34e] text-base">{slide.time}</p>
+                <Popup
+                  trigger={
+                    <div key={`slide${index}`}>
+                      <div className="box rounded-[20px] mx-[20px] my-[15px] overflow-hidden relative rounded-[20px] xl:h-[250px] lg:h-[200px] sm:h-[220px] shadow-[0_0_10px_0px_#adb5bd]">
+                        <img
+                          className="product-slide-img rounded-[20px] bg-[#fdf34e] w-full h-full align-middle object-cover"
+                          src={`${slide.img}`}
+                          alt={slide.title}
+                        />
+                        <div className="overlay w-full h-full top-1/2 left-1/2 absolute translate-x-[-50%] translate-y-[-50%] rounded-[20px]">
+                          <div className="pl-[10%]">
+                            <h3 className="text-[#fdf34e] text-base">{slide.title}</h3>
+                            <p className="text-[#fdf34e] text-base">{slide.subTitle}</p>
+                            <p className="text-[#fdf34e] text-base">{slide.time}</p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
+                  }
+                  modal
+                >
+                  {(close) => (
+                    <ContentModal
+                      closeBtn={true}
+                      close={() => false}
+                      content={
+                        <>
+                          {/* Begin Tab Menu */}
+                          <div className="w-full">
+                            <div className="flex sm:max-w-[70%] px-5 text-[#b7b7b7] font-bold mt-4 justify-between mx-auto cursor-pointer">
+                              {data[activeGroup].slides.map((slide, index) => (
+                                <div key={index} className={`mr-3 md:mr-6 min-w-fit`}></div>
+                              ))}
+                            </div>
+                            {/* End Tab Menu */}
+
+                            <div className="mt-4 mx-5 md:mx-10 scroll-box">
+                              <div className="mt-2 text-base font-bold">Chi tiết chương trình học</div>
+
+                              <div className="mt-5 text-base font-bold">Lợi ích sau khóa học:</div>
+
+                              <div className="mt-5 text-base font-bold">Cơ hội nghề nghiệp:</div>
+                            </div>
+                          </div>
+                        </>
+                      }
+                    />
+                  )}
+                </Popup>
               ))}
               {/* end slide image */}
             </Slider>
