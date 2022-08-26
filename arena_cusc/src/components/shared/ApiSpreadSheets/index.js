9e836f53ae733c008c2d31a4202c77dcf7476a3c
Hiep <hiep2001.job@gmail.com>
2022-08-26 17:25:54 +0700

    fixed merge conflictings

--- arena_cusc/src/components/shared/ApiSpreadSheets/index.js
+++ arena_cusc/src/components/shared/ApiSpreadSheets/index.js
@@ -3,8 +3,7 @@ const apispreadsheets = 'https://opensheet.elk.sh/1M1uaegvEMvVCZIcmcMFtse0rdX2sS
 // Translate Google Drive link to streamable link
 const imgFromDriveUrl = (url) => url.replace('file/d/', 'uc?export=view&id=').replace('/view?usp=sharing', '');
 
-<<<<<<< HEAD
+
 export { apispreadsheets, imgFromDriveUrl };
-=======
-export  {apispreadsheets,imgFromDriveUrl};
->>>>>>> 1ade188 (fixed header banner style and fetch data)
+
+
