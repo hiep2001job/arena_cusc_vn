631fc8f9382d5069d1a12dffb0e6e36996be01bc
sontanhoang2001 <sontanhoang2001@gmail.com>
2022-08-26 16:29:24 +0700

    fix remove function in file action

--- arena_cusc/src/actions/slide.js
+++ arena_cusc/src/actions/slide.js
@@ -1,9 +1,2 @@
 export const GET_ALL_SLIDE = 'GET_ALL_SLIDE';
 export const GET_NEXT_IMAGE = 'GET_NEXT_IMAGE';
-
-export function getAllSlide() {
-  return GET_ALL_SLIDE;
-}
-export function getNextImage() {
-  return GET_NEXT_IMAGE;
-}
