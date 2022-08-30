958941bb8d501c7a2374c492c9ec9f4621801e54
Hiep <hiep2001.job@gmail.com>
2022-08-30 11:39:55 +0700

    Fixed scrollpy

--- arena_cusc/src/App.js
+++ arena_cusc/src/App.js
@@ -20,19 +20,44 @@ function App() {
   return (
     <>
       <div className="max-w-screen-2xl mx-auto min-w-[375px]">
-        <Header />
-        <Banner />
-        <Slide />
-        <CallToAction />
-        <WhyArena />
-        <Achieverments />
-        <Instructors />
-        <AdmissionInfo />
-        <GoodStudentExamples />
-        <Curriculum />
-        <Products />
-        <News />
-        <Footer />
+      <Header />
+        {/* Wrap components in sections with id for scrollpy  */}
+        <section id="banner">
+          <Banner />
+        </section>
+        <section id="gioithieu">
+          <Slide />
+        </section>
+        <section id="ghidanh">
+          <CallToAction />
+        </section>
+        <section id="whilearena">
+          <WhyArena />
+        </section>
+        <section id="achieverments">
+          <Achieverments />
+        </section>
+        <section id="instructors">
+          <Instructors />
+        </section>
+        <section id="admissionInfo">
+          <AdmissionInfo />
+        </section>
+        <section id="goodstudent">
+          <GoodStudentExamples />
+        </section>
+        <section id="daotao">
+          <Curriculum />
+        </section>
+        <section id="sanpham">
+          <Products />
+        </section>
+        <section id="tintuc">
+          <News />
+        </section>
+        <section id="footer">
+          <Footer />
+        </section>
       </div>
     </>
   );
